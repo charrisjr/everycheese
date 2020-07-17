8689d6931dafbec61b9c94ae66c50becdfd9c780
Chuck Harris <charrisjr@gmail.com>
2020-07-17 13:55:31 -0700

    Track and display the creator of each cheese

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -21,3 +21,6 @@ class CheeseCreateView(LoginRequiredMixin, CreateView):
 		'firmness',
 		'country_of_origin',
 	]
+	def form_valid(self, form):
+		form.instance.creator = self.request.user
+		return super().form_valid(form)
